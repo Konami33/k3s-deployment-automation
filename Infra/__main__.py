087b982cf9db513203c065a215204c2541100b59
Konami33 <yasinarafat9889@gmail.com>
2024-06-30 01:16:55 +0600

    infra build and deploy

--- Infra/__main__.py
+++ Infra/__main__.py
@@ -1,10 +1,103 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+# Define the VPC and subnet configurations
+vpc = aws.ec2.Vpc("my-vpc",
+    cidr_block="10.0.0.0/16",
+    enable_dns_hostnames=True,
+    enable_dns_support=True,
+)
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# Create Public Subnet
+public_subnet = aws.ec2.Subnet("public-subnet",
+    vpc_id=vpc.id,
+    cidr_block="10.0.1.0/24",
+    map_public_ip_on_launch=True,
+    availability_zone="ap-southeast-1a",
+)
+
+# Create Internet Gateway
+igw = aws.ec2.InternetGateway("igw",
+    vpc_id=vpc.id,
+)
+
+# Create Route Table
+route_table = aws.ec2.RouteTable("route-table",
+    vpc_id=vpc.id,
+    routes=[{
+        "cidr_block": "0.0.0.0/0",
+        "gateway_id": igw.id,
+    }],
+)
+
+# Associate Route Table with Public Subnet
+rt_assoc_public = aws.ec2.RouteTableAssociation("rt-assoc-public",
+    subnet_id=public_subnet.id,
+    route_table_id=route_table.id,
+)
+
+# Create Security Group
+security_group = aws.ec2.SecurityGroup("web-secgrp",
+    description='Enable SSH and K3s access',
+    vpc_id=vpc.id,
+    ingress=[
+        {
+            "protocol": "tcp",
+            "from_port": 22,
+            "to_port": 22,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+        {
+            "protocol": "tcp",
+            "from_port": 6443,
+            "to_port": 6443,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+    ],
+    egress=[{
+        "protocol": "-1",
+        "from_port": 0,
+        "to_port": 0,
+        "cidr_blocks": ["0.0.0.0/0"],
+    }],
+)
+
+ami_id = "ami-003c463c8207b4dfa"  # Replace with a valid AMI ID for your region
+instance_type = "t3.small"
+
+master_node = aws.ec2.Instance("master-node",
+    instance_type=instance_type,
+    ami=ami_id,
+    subnet_id=public_subnet.id,
+    key_name="MyKeyPair",
+    vpc_security_group_ids=[security_group.id],
+    tags={
+        "Name": "master-node"
+    })
+
+
+worker_node_1 = aws.ec2.Instance("worker-node-1",
+    instance_type=instance_type,
+    ami=ami_id,
+    subnet_id=public_subnet.id,
+    key_name="MyKeyPair",
+    vpc_security_group_ids=[security_group.id],
+    tags={
+        "Name": "worker-node-1"
+    })
+
+worker_node_2 = aws.ec2.Instance("worker-node-2",
+    instance_type=instance_type,
+    ami=ami_id,
+    subnet_id=public_subnet.id,
+    key_name="MyKeyPair",
+    vpc_security_group_ids=[security_group.id],
+    tags={
+        "Name": "worker-node-2"
+    })
+
+# Export outputs
+pulumi.export("master_public_ip", master_node.public_ip)
+pulumi.export("worker1_public_ip", worker_node_1.public_ip)
+pulumi.export("worker2_public_ip", worker_node_2.public_ip)
+
